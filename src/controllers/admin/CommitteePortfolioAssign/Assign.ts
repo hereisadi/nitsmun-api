1cc347ce8099a3bd273fbb8fcc91caa6ef45555e
hereisadi <hereisadi@proton.me>
2024-01-29 20:50:41 +0530

    UPD:minor changes

--- src/controllers/admin/CommitteePortfolioAssign/Assign.ts
+++ src/controllers/admin/CommitteePortfolioAssign/Assign.ts
@@ -69,13 +69,72 @@ export const assignPortfolios = async (req: AuthRequest, res: Response) => {
           event.assignedCommittee = committee;
         }
         await event.save();
-        sendEmail(
-          event.email,
-          "[NITSMUN] Committee and Portfolio assigned",
-          `Hi ${event.name},\n
-          We are glad to inform you that you have been assigned ${committee} committee and ${portfolio} portfolio for the ${event.eventName}. \n Thanks,
-          \n\n Team NITSMUN`
-        );
+
+        const ibWpGrp = "https://chat.whatsapp.com/CkmW628dU1c6kxsctt98GJ";
+        const mahabharatWpGrp =
+          "https://chat.whatsapp.com/L24IHRg4kueKmbdMpG9fWf";
+        const unhrcWpGrp = "https://chat.whatsapp.com/IwQiJMKGfrFJta8QrAS5bd";
+        const ipcWpGrp = "https://chat.whatsapp.com/Fo9tj6KaRqH9iuy5IvwiNV";
+
+        const ibBgGuide = "ib";
+        const mahabharatBgGuide = "maha";
+        const unhrcBgGuide = "unhrc";
+        const ipcBgGuide = "ipc";
+
+        if (committee === "Mahabharat" || committee === "mahabharat") {
+          sendEmail(
+            event.email,
+            "[NITSMUN] Committee and Portfolio assigned",
+            `Hi ${event.name},\n
+            We are glad to inform you that you have been assigned ${committee} committee and ${portfolio} portfolio for the ${event.eventName}. \n\n Please join the Hastinapur Special Council/Mahabharat Whatsapp group: ${mahabharatWpGrp} \n\n Below is the Background guide of the Hastinapur Special Council/Mahabharat Committee: \n${mahabharatBgGuide} \n Thanks,
+            \n\n Team NITSMUN`
+          );
+        } else if (
+          committee === "IB" ||
+          committee === "ib" ||
+          committee === "Ib"
+        ) {
+          sendEmail(
+            event.email,
+            "[NITSMUN] Committee and Portfolio assigned",
+            `Hi ${event.name},\n
+            We are glad to inform you that you have been assigned ${committee} committee and ${portfolio} portfolio for the ${event.eventName}. \n\n Please join the IB Committee Whatsapp group: ${ibWpGrp} \n\n Below is the Background guide of the IB Committee: \n${ibBgGuide} \n Thanks,
+            \n\n Team NITSMUN`
+          );
+        } else if (
+          committee === "IPC" ||
+          committee === "ipc" ||
+          committee === "Ipc"
+        ) {
+          sendEmail(
+            event.email,
+            "[NITSMUN] Committee and Portfolio assigned",
+            `Hi ${event.name},\n
+            We are glad to inform you that you have been assigned ${committee} committee and ${portfolio} portfolio for the ${event.eventName}. \n\n Please join the IPC Committee Whatsapp group: ${ipcWpGrp} \n\n Below is the Background guide of the IPC Committee: \n${ipcBgGuide} \n Thanks,
+            \n\n Team NITSMUN`
+          );
+        } else if (
+          committee === "UNHRC" ||
+          committee === "unhrc" ||
+          committee === "Unhrc"
+        ) {
+          sendEmail(
+            event.email,
+            "[NITSMUN] Committee and Portfolio assigned",
+            `Hi ${event.name},\n
+            We are glad to inform you that you have been assigned ${committee} committee and ${portfolio} portfolio for the ${event.eventName}. \n\n Please join the UNHRC Committee Whatsapp group: ${unhrcWpGrp} \n\n Below is the Background guide of the UNHRC Committee: \n${unhrcBgGuide} \n Thanks,
+            \n\n Team NITSMUN`
+          );
+        } else {
+          sendEmail(
+            event.email,
+            "[NITSMUN] Committee and Portfolio assigned",
+            `Hi ${event.name},\n
+            We are glad to inform you that you have been assigned ${committee} committee and ${portfolio} portfolio for the ${event.eventName}. \n Thanks,
+            \n\n Team NITSMUN`
+          );
+        }
+
         return res
           .status(200)
           .json({ success: true, message: "Portfolio and committee updated" });
